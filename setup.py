a18330e6eec20e63d174afdcc5de46436acc4bc1
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-23 20:07:17 +0300

    init

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='django-postgres-vacuum',
-    version='2020.12.8',
+    version='2020.12.24',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
